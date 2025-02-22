7b4a28c476aec008fb98e61178b6c7ac2cca1e54
Diego Lopes Sakata <dihsakata@gmail.com>
2025-02-22 16:50:53 -0300

    feat: implementa a funcao menu

--- intro_git/menu.py
+++ intro_git/menu.py
@@ -4,3 +4,11 @@ def entradaValores():
     a=int(input("Por favor, digite o primeiro numero da operacao: "))
     b=int(input("Digite o segundo numero: "))
     return a, b
+
+def menu():
+    menu = {}
+    menu['1']="Somar"
+    menu['2']="Subtrair"
+    menu['3']="Multiplicar"
+    menu['4']="Dividir"
+    menu['5']="Sair do sistema"
